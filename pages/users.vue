ba38e667eb7767d460ada69bf34d076552091c1c
degfra <degfra2@gmail.com>
2018-03-09 18:04:34 +0100

    nested routes

---
+++ pages/users.vue
@@ -0,0 +1,27 @@
+<!-- nuxt-child in a /pages/file.vue : 
+     will display the content of any sub routes of the /pages/users folder :
+     => more generally, any subroute of the /pages/<subfolder>
+     whose name == the file name of the .vue file where nuxt-child resides -->
+
+<template>
+  <div>
+    <input type="text" v-model="userId">
+    <button @click="onLoadUser">Load User</button>
+    <nuxt-child />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      userId: ''
+    }
+  },
+  methods: {
+    onLoadUser() {
+      this.$router.push('/users/' + this.userId)
+    }
+  }
+}
+</script>
--- pages/users/_id/index.vue
+++ pages/users/_id/index.vue
@@ -1,3 +1,14 @@
 <template>
   <h1>A single user, with ID: {{ $route.params.id }}</h1>
 </template>
+
+<script>
+export default {
+  validate (data) {
+    // console.log(data)
+    // return data.params.id == 1
+    return /^\d+$/.test(data.params.id)
+  }
+}
+</script>
+
